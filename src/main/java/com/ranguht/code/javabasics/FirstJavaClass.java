6d0d712d810372ab586cf4f48d5ac5f71ffd01e1
Naresh <naresh_rangu@epam.com>
2019-07-05 12:10:18 +0530

    Commit java pgm

--- src/main/java/com/ranguht/code/javabasics/FirstJavaClass.java
+++ src/main/java/com/ranguht/code/javabasics/FirstJavaClass.java
@@ -1,4 +1,8 @@
-package com.ranguht.code;
-
+package com.ranguht.code.javabasics;
 public class FirstJavaClass {
+    public static void main(String[] args) {
+        System.out.println("*****************");
+        System.out.println("Hey Rangu");
+        System.out.println("****************");
+    }
 }
